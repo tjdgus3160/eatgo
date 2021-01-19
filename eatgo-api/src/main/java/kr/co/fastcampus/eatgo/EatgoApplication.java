2c6abcdf60a3663b585384c199b766cabc880e79
tjdgus3160 <tjdgus3160@gmail.com>
2021-01-19 23:59:48 +0900

    test

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EatgoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("a");
 		SpringApplication.run(EatgoApplication.class, args);
 	}
 
